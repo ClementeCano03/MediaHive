139082f21192fac409e22f600d26737d55501293
Jose Antonio Casado Molina <124878375+jcasadomolina@users.noreply.github.com>
2024-04-30 20:04:44 +0200

    Add files via upload

---
+++ MediaHive_code/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TopNavBar from "./Components/TopNavBar";
+import LeftToolBar from "./Components/LeftToolBar";
+
+function App(){
+    return(
+        <>
+            <TopNavBar/>
+           
+        </>
+    )
+}
+
+export default App
