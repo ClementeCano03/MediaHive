7fede45203b82c0707582eb2327a265734370df1
Cano <clementecano2003@gmail.com>
2024-05-01 12:38:02 +0200

    Actualizacion Barras

--- MediaHive_code/MediaHive/src/Components/TopNavBar.jsx
+++ MediaHive_code/MediaHive/src/Components/TopNavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import imagen from "../images/MediaHive_icon.png";
-import { Link } from 'react-router-dom';
+
 
 import './TopNavBar.css';
 
@@ -8,9 +8,15 @@ function TopNavBar(){
     return(
         <nav className="navbar">
             <div className="container-fluid align-items-center">
+<<<<<<< Updated upstream
                 <img className="imagen" src={imagen} href="home.jsx" alt="Imagen de la web"/>
                 <h2>MediaHive</h2>
                 <h3 className="tituloPagina">Inicio</h3>
+=======
+                <img className="imagen" src={imagen} alt="Imagen de la web"/>
+                <h2>MediaHive</h2>
+                <h3 className="tituloPagina"></h3>
+>>>>>>> Stashed changes
                 <h5 className="registro">Registrarse | Inicar Sesión</h5>
             </div>
         </nav>
