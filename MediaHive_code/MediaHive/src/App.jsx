0fd7880f60c758b1a1ed58a999bde3d106efb503
Cano <clementecano2003@gmail.com>
2024-05-01 13:02:27 +0200

    Update App.jsx

--- MediaHive_code/MediaHive/src/App.jsx
+++ MediaHive_code/MediaHive/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import TopNavBar from "./Components/TopNavBar";
 import LeftToolBar from "./Components/LeftToolBar";
+import Home from "./Components/Home";
 
 function App(){
   return(
     <>
-      <TopNavBar />
-      <LeftToolBar />
+      <Home/>
     </>
   )
 }
