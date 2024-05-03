178a3a406b28302bb5a7251cdbb951a4637674c0
ManuelFuentes03 <manuelfuentes@uma.es>
2024-05-03 14:56:16 +0200

    Implementado buscador de canciones y reproductor con API

--- MediaHive_code/MediaHive/src/Components/Musica.jsx
+++ MediaHive_code/MediaHive/src/Components/Musica.jsx
@@ -1,40 +1,104 @@
-import React from "react";
-import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+
+import LeftToolBar from "./LeftToolBar";
+import TopNavBar from "./TopNavBar";
+
+//import "../styles/Musica.css"
 
 function Musica(/*{ cambiarTituloPagina }*/) {
   //cambiarTituloPagina("Música"); // Cambia el título de la página al cargar este componente
 
+  const [cancion, setCancion] = useState('')
+  const [canciones, setCanciones] = useState([])
+
+  function handleSearch(e) {
+    e.preventDefault();
+
+    if(cancion.trim() === ''){
+      alert('Debes ingresar una canción');
+      return;
+    }
+
+    console.log(cancion);
+    setCancion("");
+    getSong(cancion);
+  }
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '733197fdcamsh9fd898d4b4b0d10p1493cdjsn7aa3fb3dcb4d',
+      'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
+  };
+
+  async function getSong(cancion) {
+    try {
+      let url = `https://spotify23.p.rapidapi.com/search/?q=${cancion}&type=multi&offset=0&limit=10&numberOfTopResults=5`;
+      let data = await fetch(url, options);
+      let res = await data.json();
+      setCanciones(res.tracks.items);
+      console.log(res);
+
+    } catch (error) {
+      console.log(`ERROR: ${error}`);
+    }
+  };
+
+
   return (
-    <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-      {/* Contenedor de la canción */}
-      <iframe
-        src="https://open.spotify.com/embed/track/3cg2Y9mIiYNdcPTLKaQgK3?utm_source=generator"
-        width="30%"
-        height="352"
-        frameBorder="0"
-        allowFullScreen=""
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></iframe>
+    <>
+      {/*Barra de buscador*/}
+      <form onSubmit={handleSearch} className="formulario"style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <input type="text" value={cancion} onChange={e => setCancion(e.target.value)} style={{
+          padding: '10px',
+          borderRadius: '5px',
+          border: '2px solid #ccc',
+          marginRight: '10px',
+          fontSize: '16px',
+          outline: 'none',
+        }}
+        placeholder="Buscar canción..."
+        />
+        <button type="submit" style={{
+            padding: '10px 20px',
+            borderRadius: '5px',
+            border: 'none',
+            backgroundColor: '#455559',
+            color: 'white',
+            fontSize: '16px',
+            cursor: 'pointer',
+            transition: 'background-color 0.3s',
+          }}
+        >Buscar</button>
+      </form>
 
-      {/* Contenedor del título y el botón */}
-      <div style={{ marginLeft: "1rem", display: "flex", flexDirection: "column", alignItems: "flex-start", justifyContent: "space-between" }}>
-        {/* Título de la canción con emoticono */}
-        <h3 style={{ marginBottom: "0.5rem" }}>
-          Titulo Cancion <BookmarkAddIcon style={{ marginLeft: "0.5rem" }}/>
-        </h3>
+      {/*Resultados*/}
+      {canciones.map((cancion, index) => (
+          <div key={index}>
+            <img src={cancion.data.albumOfTrack.coverArt.sources[0].url}/>
+            <h2 style={{color:"black"}}>
+              {cancion.data.name}
+            </h2>
+            <a href={cancion.data.uri}>
+              <button>
+                Abrir canción en spotify
+              </button>
+            </a>
+            <iframe
+              src={`https://open.spotify.com/embed/track/${cancion.data.id}?utm_source=generator`}
+              width="30%"
+              height="352"
+              frameBorder="0"
+              allowFullScreen=""
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+            ></iframe>
+          </div>
+      ))}
 
-        {/* Estrellas */}
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <StarBorderIcon/><StarBorderIcon/><StarBorderIcon/><StarBorderIcon/>
-        </div>
-      </div>
-
-      {/* Botón para añadir comentario */}
-      <Button variant="contained" color="primary">Añadir comentario</Button>
-    </div>
+    </>
+    
   );
 }
 
