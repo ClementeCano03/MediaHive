f495a1e1ab2f2b8511b06e54c7d00660ee69564e
Juanma <0610720075@uma.es>
2024-05-03 13:22:09 +0200

    Diseño peliculas
    Diseño general sin imagenes de la pestaña de peliculas y paquetes nuevos en json

--- MediaHive_code/MediaHive/src/Components/Peliculas.jsx
+++ MediaHive_code/MediaHive/src/Components/Peliculas.jsx
@@ -1,14 +1,212 @@
 import React from "react";
+import { Carousel } from "react-bootstrap";
+import { useRef } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import "../styles/peliculas-styles.css";
 
 function Peliculas({ cambiarTituloPagina }) {
-  cambiarTituloPagina("Películas"); // Cambia el título de la página al cargar este componente
+  //cambiarTituloPagina("Películas");
+
+  // Ref para el carrusel
+  const carouselRef = useRef(null);
 
   return (
-    <div>
-      <h1>Página de Películas</h1>
-      <p>Aquí puedes ver nuestras películas disponibles.</p>
+    <div id="peliculas" className="d-flex flex-column flex-grow-1">
+      <h1 class="visually-hidden">Página de Películas</h1>
+
+      <form id="buscadorPelis" class="d-flex justify-content-center mx-auto py-5 w-75">
+        <input type="search" placeholder="Buscar..." className="flex-grow-1 "></input>
+        <button type="send">Buscar</button>
+      </form>
+
+      {/* Carrusel de peliculas populares*/}
+      <div id="populares" className="carousel-container mx-auto px-5 py-3">
+        <h2>Populares</h2>
+        <Carousel ref={carouselRef} interval={null} indicators={false}>
+          <Carousel.Item>
+            <div className="carousel-item-content row align-items-center py-2">
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="carousel-item-content row align-items-center py-2">
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+
+      {/* Carrusel de top 10 peliculas*/}
+      <div id="top10" className="carousel-container mx-auto px-5 py-3">
+        <h2>Top 10 peliculas</h2>
+        <Carousel ref={carouselRef} interval={null} indicators={false}>
+          <Carousel.Item>
+            <div className="carousel-item-content row align-items-center py-2">
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="carousel-item-content row align-items-center py-2">
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+
+      {/* Carrusel de guardadas*/}
+      <div id="guardadas" className="carousel-container mx-auto px-5 py-3">
+        <h2>Guardadas</h2>
+        <Carousel ref={carouselRef} interval={null} indicators={false}>
+          <Carousel.Item>
+            <div className="carousel-item-content row align-items-center py-2">
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="carousel-item-content row align-items-center py-2">
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col">
+                <img src="imagen.jpg" alt="Imagen de la película" style={{ width: '150px', height: 'auto' }}/>
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+
+      {/* Carrusel de estrenos*/}
+      <div id="estrenos" className="carousel-container mx-auto px-5 py-3">
+        <h2>Estrenos</h2>
+        <Carousel ref={carouselRef} interval={null} indicators={false}>
+          <Carousel.Item>
+            <div className="carousel-item-content row align-items-center py-2">
+              <div className="col example-content">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col example-content">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col example-content">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col example-content">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col example-content">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="carousel-item-content row align-items-center py-2">
+              <div className="col example-content">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col example-content">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col example-content">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col example-content">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+              <div className="col example-content">
+                <img src="imagen.jpg" alt="Imagen de la película" />
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default Peliculas;
