f8ae7919923a83f8befd9a57bb2b71468ba35e19
ManuelFuentes03 <manuelfuentes@uma.es>
2024-05-03 13:35:14 +0200

    Añadida ruta música

--- MediaHive_code/MediaHive/src/App.jsx
+++ MediaHive_code/MediaHive/src/App.jsx
@@ -2,13 +2,27 @@ import React, {useEffect, useState} from "react";
 import { Routes, Route } from 'react-router-dom';
 import Inicio from "./Components/Inicio";
 import Musica from "./Components/Musica";
+import LeftToolBar from "./Components/LeftToolBar";
+import TopNavBar from "./Components/TopNavBar";
 
 function App(){
   return(
     <>
       <Routes>
-        <Route path='/' element={<Inicio/>}/>
-        <Route path='/musica' element={<Musica/>}/>
+        <Route path='/' element={
+          <>
+            <TopNavBar/>
+            <div style={{display: "flex"}}>
+              <LeftToolBar/>
+              <div style={{ flex: 1}}>
+              <Inicio/>
+              </div>
+            </div>
+          </>
+        }/>
+        <Route path='/musica' element={
+          <Musica/>
+        }/>
       </Routes>
     </>
   );
