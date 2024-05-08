8abd7bb8fd5bb25a654175e48d006ce92b615a9c
Juanma <0610720075@uma.es>
2024-05-08 11:21:48 +0200

    DetallesPeliculasSeries
    Documentos base de la pagina citada

---
+++ MediaHive_code/MediaHive/src/Components/DetallesPeliculasSeries.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState, useRef } from "react";
+import axios from 'axios'
+
+import { Carousel } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "../styles/detallesPeliculasSeries-styles.css";
+
+function DetallesPeliculasSeries({ cambiarTituloPagina }) {
+    return(
+        <div></div>
+    )
+}
+
+
+export default DetallesPeliculasSeries;
