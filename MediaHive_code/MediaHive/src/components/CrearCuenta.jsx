c1630537cd8ca0dc152133e1221785803ab33959
MVictoriaHuesca <victoriahuesca@uma.es>
2024-05-12 19:06:01 +0200

    se muestran errores si el formulario de crear cuenta es incorrecto

--- MediaHive_code/MediaHive/src/components/CrearCuenta.jsx
+++ MediaHive_code/MediaHive/src/components/CrearCuenta.jsx
@@ -1,40 +1,97 @@
 import React from "react";
 import '../Styles/CrearCuenta.css';
 import imagen from "../Images/MediaHive_icon.png";
-import { Button } from '@mui/material';
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function CrearCuenta() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  console.log(errors);
+  const onSubmit = handleSubmit(data => {
+    console.log(data)
+  })
 
-  return(
+  return (
     <div className="content-crear-cuenta">
       <link href='https://fonts.googleapis.com/css?family=Livvic' rel='stylesheet'></link>
-      
-        <div className="crear-cuenta-container">
-          <img className="imagen-crear-cuenta" src={imagen} alt="Icono de MediaHive"/>
-          <div class="form-container">
-            <label for="nombreUsuario" class="label-crear-cuenta">Nombre de usuario</label>
-            <input type="text" name="nombreUsuario" maxLength={20} required class="input-crear-cuenta"/>
-            <label for="correoElectrónico" class="label-crear-cuenta">Correo electrónico</label>
-            <input type="email" name="correoElectrónico" required class="input-crear-cuenta"/>
-            <label for="contraseña" class="label-crear-cuenta">Contraseña</label>
-            <input type="password" name="contraseña" required class="input-crear-cuenta"/>
-          </div>
-          <Button 
-            component={Link} 
-            to="/Inicio" 
-            variant="contained" 
-            sx={{
-              backgroundColor: '#f0cb06', 
-              '&:hover': {
-                backgroundColor: '#0a6f94', 
-                color: 'black', 
-              }
-            }}
-            className="boton-aceptar">
-              ACEPTAR
-          </Button>
-        </div>
+      <div className="crear-cuenta-container">
+        <img className="imagen-crear-cuenta" src={imagen} alt="Icono de MediaHive"/>
+          <form onSubmit={ onSubmit } className="form-container">
+            <label htmlFor="nombreUsuario" className="label-crear-cuenta">Nombre de usuario</label>
+            <input
+              type="text"
+              name="nombreUsuario"
+              {...register("nombreUsuario", { 
+                required: {
+                  value: true, 
+                  message: 'Nombre de usuario obligatorio'
+                },
+                maxLength: {
+                  value: 20,
+                  message: 'Nombre de usuario demasiado largo'
+                },
+                minLength: {
+                  value: 4,
+                  message: 'Nombre de usuario demasiado corto'
+                }
+              })}
+              className="input-crear-cuenta"
+            />
+            {
+              errors.nombreUsuario && <span className='span-crearcuenta'>{errors.nombreUsuario.message}</span>
+            }
+            <label htmlFor="correoElectronico" className="label-crear-cuenta">Correo electrónico</  label>
+            <input
+              type="email"
+              name="correoElectronico"
+              {...register("correoElectronico", { 
+                required: {
+                  value: true,
+                  message: 'Correo electrónico obligatorio'
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                  message: 'Correo no válido'
+                }
+              })}
+              className="input-crear-cuenta"
+            />
+            {
+              errors.correoElectronico && <span className='span-crearcuenta'>{errors.correoElectronico.message}</span>
+            }
+            <label htmlFor="contrasena" className="label-crear-cuenta">Contraseña</label>
+            <input
+              type="password"
+              name="contrasena"
+              {...register("contrasena", { 
+                required: {
+                  value: true, 
+                  message: 'Contraseña obligatoria'
+               },
+               minLength: {
+                 value: 4,
+                 message: 'Contraseña debe tener al menos 4 caracteres'
+               },
+               maxLength: {
+                  value: 20,
+                  message: 'Contraseña debe tener menos de 20 caracteres'
+               }
+              })}
+              className="input-crear-cuenta"
+            />
+            {
+              errors.contrasena && <span className='span-crearcuenta'>{errors.contrasena.message}</span>
+            }
+            
+              <button
+                type="submit"
+                to="/Inicio"
+                className="boton-aceptar">
+                ACEPTAR
+              </button>
+          
+          </form>
+        
+      </div>
     </div>
   );
 }
