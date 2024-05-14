a413b56f405697eac3d60d03e67b0dad8b587d2f
Juanma <0610720075@uma.es>
2024-05-14 21:10:36 +0200

    Esqueleto comentarios

--- MediaHive_code/MediaHive/src/Components/detallesSeries.jsx
+++ MediaHive_code/MediaHive/src/Components/detallesSeries.jsx
@@ -12,6 +12,17 @@ function detallesSeries({ cambiarTituloPagina }) {
     const language = 'es-ES';
     const [similarSeries, setSimilarSeries] = useState([]);
 
+    const [comments, setComments] = useState([]);
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const comment = event.target.elements[0].value;
+        setComments([comment, ...comments]);
+        event.target.reset();
+    };
+
+
     useEffect(() => {
         const fetchSerie = async () => {
             const { data } = await axios.get(
@@ -24,6 +35,8 @@ function detallesSeries({ cambiarTituloPagina }) {
             const response = await axios.get(`https://api.themoviedb.org/3/tv/${id}/similar?api_key=fd04580a5174281296d7de8867bc1fa0&language=~${language}`);
             setSimilarSeries(response.data.results);
         };
+
+        
 
         fetchSerie();
         fetchSimilarSeries();
@@ -71,7 +84,24 @@ function detallesSeries({ cambiarTituloPagina }) {
 
                 </div>
 
+                
+
             </div>
+            <div>
+                    <h3 className="mx-auto py-3 px-5">Comentarios</h3>
+                    <div className="mx-auto px-5 py-3">
+                        {comments.map((comment, index) => (
+                            <div key={index} style={{ border: '1px solid black', margin: '10px 0', padding: '10px' }}>
+                                {comment}
+                            </div>
+                        ))}
+                    </div>
+                    <h3 className="mx-auto py-3" style={{ textAlign: 'center' }}>Añadir un comentario</h3>
+                    <form onSubmit={handleSubmit} style={{ padding: '0 50px' }}>
+                        <textarea placeholder="Escribe tu comentario aquí..." style={{ width: '100%', height: '100px', padding: '10px', resize: 'none' }}></textarea>
+                        <button type="submit" style={{ display: 'block', margin: '10px auto' }}>Enviar</button>
+                    </form>
+                </div>
         </div>
     )
 }
